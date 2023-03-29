2b6bbc55117a322ef5f187a49dfceeb3bc06970c
Manickam Somasundaram <manickam.som@gmail.com>
2023-03-29 17:58:00 +0200

    recommit for new run

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import './App.css';
 import './nprogress.css';
+
 import CitySearch from './CitySearch';
 import NumberOfEvents from './NumberOfEvents';
 import EventList from './EventList';
