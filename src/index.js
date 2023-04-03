e32c35500ea3ac48ec111863fab8f4f7f176797f
Manickam Somasundaram <manickam.som@gmail.com>
2023-04-03 21:12:10 +0200

    atatus monitoring configured

--- src/index.js
+++ src/index.js
@@ -6,6 +6,9 @@ import App from './App';
 
 import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 import reportWebVitals from './reportWebVitals';
+import * as atatus from 'atatus-spa';
+
+atatus.config('b840e8ecf87c481c9b47e73806b44baf').install();
 
 ReactDOM.render(
   <React.StrictMode>
@@ -23,3 +26,5 @@ serviceWorkerRegistration.register();
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
+atatus.notify(new Error('Test Atatus Setup'))
